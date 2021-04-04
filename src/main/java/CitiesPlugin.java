34eba3eb876b274a8cb1161af97b94b56a699699
8vogt <8vogt@informatik.uni-hamburg.de>
2021-04-04 15:56:53 +0200

    Initial data commit of sources and pom

---
+++ src/main/java/CitiesPlugin.java
@@ -0,0 +1,14 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CitiesPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
