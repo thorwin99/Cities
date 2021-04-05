d5d282685e41f3ad57683b8bec1dffc955bf9734
8vogt <8vogt@informatik.uni-hamburg.de>
2021-04-05 14:24:02 +0200

    Added methods to interact with cities using the city manager for save interaction

--- src/main/java/Main/CityManager.java
+++ src/main/java/Main/CityManager.java
@@ -2,6 +2,7 @@ package Main;
 
 import Serilazibles.City;
 import Serilazibles.Vector2;
+import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
@@ -32,7 +33,8 @@ public class CityManager {
     private HashMap<String, City> cities;
 
     /**
-     * Map of chunks and their data, used to store custom chunk data
+     * Map of chunks and their data, used to store custom chunk data.
+     * Only chunks which are part of a city are currently in here
      */
     private HashMap<Chunk, ChunkData> ownedChunks;
 
@@ -63,7 +65,7 @@ public class CityManager {
      * @return If the city was created successfully, false if one already exists with that name
      */
     public boolean createCity(String cityName, Player creator, HashSet<Chunk> cityChunks){
-        if(cities.containsKey(cityName.toLowerCase())) return false;
+        if(cityExists(cityName)) return false;
 
         City city = new City(cityName, creator.getUniqueId(), creator.getWorld());
 
@@ -77,6 +79,121 @@ public class CityManager {
         cities.put(cityName.toLowerCase(), city);
         SaveCities();
         return true;
+    }
+
+    /**
+     * Removes the given city from the world.
+     * @param cityName Name of the city
+     * @return True, if city was removed, false if no such city exist
+     */
+    public boolean removeCity(String cityName){
+        if(!cityExists(cityName))return false;
+        City city = cities.get(cityName.toLowerCase());
+        World world = Bukkit.getWorld(city.getCityWorld());
+        if(world == null){
+            logger.warning("The city " + cityName + " is no longer in a valid world. It will still be removed, its chunks wont be updated");
+            cities.remove(cityName.toLowerCase());
+            return true;
+        }
+
+        for(Vector2 chunk : city.getChunks()){
+            Chunk c = world.getChunkAt(chunk.X, chunk.Y);
+            ownedChunks.remove(c); //TODO: Change to more complex chunk system if needed, since this is way to simple
+        }
+
+        cities.remove(cityName.toLowerCase());
+
+        return true;
+    }
+
+    /**
+     * Adds a new resident to a city, if the player is not a resident in another city.
+     * @param cityName City to add the player as a resident
+     * @param resident Player to add as resident
+     * @return True, if player was added, false if he is already a resident of another city or the city does not exist
+     */
+    public boolean addResidentToCity(String cityName, Player resident) {
+        if(!cityExists(cityName))return false;
+        if(getPlayerCity(resident) != null)return false;
+
+        City city = cities.get(cityName.toLowerCase());
+        city.addResident(resident.getUniqueId());
+        return true;
+    }
+
+    /**
+     * Removes the player from a cities residents.
+     * @param cityName Name of the city
+     * @param resident Player to remove
+     * @return True, if removed, false if city does not exist or the player is no resident there.
+     */
+    public boolean removeResidentFromCity(String cityName, Player resident){
+        if(!cityExists(cityName))return false;
+        City city = cities.get(cityName);
+
+        return city.getResidents().remove(resident.getUniqueId());
+    }
+
+    /**
+     * Adds the given chunk to the given cities area, if it is not part of another city.
+     * @param chunk Chunk to add
+     * @param cityName Name of the city
+     * @return True, if added, false if either the chunk is already part of a city, or the city does not exist.
+     */
+    public boolean addChunkToCity(Chunk chunk, String cityName){
+        if(!cityExists(cityName))return false;
+        if(ownedChunks.containsKey(chunk))return false;
+        City city = cities.get(cityName.toLowerCase());
+        ChunkData data = new ChunkData(chunk, city);
+        ownedChunks.put(chunk, data);
+        city.addChunk(chunk);
+
+        return true;
+    }
+
+    /**
+     * Removes a chunk from a city, if it is in its area.
+     * @param chunk The chunk to remove from the city
+     * @param cityName The city.
+     * @return True, if removed, false if city does not exist or chunk has no data associated with
+     */
+    public boolean removeChunkFromCity(Chunk chunk, String cityName){
+        if(!cityExists(cityName))return false;
+        if(!ownedChunks.containsKey(chunk))return false;
+
+        ChunkData data = ownedChunks.get(chunk);
+
+        if(data.getCity() == null)return false;
+
+        City city = cities.get(cityName);
+
+        boolean ret = city.removeChunk(chunk);
+        ownedChunks.remove(chunk);
+
+        return ret;
+    }
+
+    /**
+     * Tries to get the city of the player, where he resides.
+     * @param resident The player to get the city for
+     * @return The name of the city of the player or null, if he is no resident
+     */
+    public String getPlayerCity(Player resident){
+        for(City city : cities.values()){
+            if(city.getResidents().contains(resident.getUniqueId())){
+                return city.getName();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Checks if the given city exists
+     * @param cityName Name of the city
+     * @return True if tit exists
+     */
+    public boolean cityExists(String cityName){
+        return cities.containsKey(cityName.toLowerCase());
     }
 
     /**
